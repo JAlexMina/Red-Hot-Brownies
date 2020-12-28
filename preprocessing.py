90bd91be05c5a78d056580a13670491d5a5c9a65
J. Alex Mina <juanmina1200@gmail.com>
2020-12-28 18:32:47 -0500

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,28 @@
 import re
 import csv
 
+class item: 
+    def __init__(self, attribs): 
+        self.attribs = attribs
+        
+    def __eq__(self, other):
+        if(len(self.attribs) == len(other.attribs)):
+            keys = self.attribs.keys()
+            for key in keys:
+                try:
+                    if self.attribs[key] != other.attribs[key] and key != "index":
+                        return False
+                except:
+                    return False
+            return True
+        else: 
+            return False
+        
+    def printItem(self):
+        print(attribs["index"])
 
-with open("mlchallenge_set_2021.tsv", encoding="latin-1") as tsvfile:
+
+with open("../mlchallenge_set_2021.tsv", encoding="latin-1") as tsvfile:
     tsvreader = csv.reader((x.replace('\0', '') for x in tsvfile), delimiter="\t", quotechar='"')
     i = 0
     items = list()
@@ -17,8 +37,8 @@ with open("mlchallenge_set_2021.tsv", encoding="latin-1") as tsvfile:
 
             if i == 9:
                 attribs["category"] = "2"
-            else:
-                i += 1
+
+                
             
 
             try:
@@ -26,43 +46,112 @@ with open("mlchallenge_set_2021.tsv", encoding="latin-1") as tsvfile:
                 pass
             except:
                 pass
+            
             try:
                 attribs["brand"] = re.split(":", re.search("(?i)brand[^:]*:[^,]*,", description).group())[1][:-1]                       
                 pass
             except:
                 pass
+            
             try:
                 attribs["color"] = re.split(":", re.search("(?i)color[^:]*:[^,]*,", description).group())[1][:-1]                        
                 pass
             except:
                 pass
+            
             try:
                 attribs["width"] = re.split(":", re.search("(?i)width[^:]*:[^,]*,", description).group())[1][:-1]                          
                 pass
             except:
                 pass
+            
             try:
                 attribs["material"] = re.split(":", re.search("(?i)material[^:]*:[^,]*,", description).group())[1][:-1]
                 pass
             except:
                 pass
+            
             try:
                 attribs["style"] = re.split(":", re.search("(?i)style[^:]*:[^,]*,", description).group())[1][:-1]
                 pass
             except:
                 pass
+
+            try:
+                attribs["style 2"] = re.split(":", re.search("(?i)style 2[^:]*:[^,]*,", description).group())[1][:-1]
+                pass
+            except:
+                pass
                     
 
-            #print(attribs)
-            items.append(attribs)
 
-            if attribs("category") == "4":
-                print(description)
+
+            thing = item(attribs)
+            items.append(thing)
             
             
             pass
         except:
             pass
+
+
+        i += 1
+
+        if i == 2500:
+            break
+
+print("done with regEx")
+
+groups = list()
+skiplst = list()
+
+i = 0
+lin = len(items)
+
+
+for item1 in items:
+    if item1 in skiplst:
+        pass
+    else:
+        skiplst.append(item1)
+        for item2 in items:
+            if item2 in skiplst and skiplst[skiplst.index(item2)].attribs["index"] == item2.attribs["index"]:
+                pass
+            else:
+                try:
+                    if item1 in groups[-1]:
+                        pass
+                    else:
+                        groups.append([item1]) 
+                except:
+                    groups.append([item1])#group is empty
+                    
+                
+                if item1 == item2:
+                     #print("NEVER")
+                     groups[-1].append(item2)
+                     skiplst.append(item2)
+    print((i/lin)*100, "%")
+    i += 1
+    
+        
+    
+ 
+    
+
+##intt = 0
+##for group in groups:
+##    if len(group) > 1:
+##        print("group ", intt, ": \n{")
+##        for item in group:
+##            print("     ", item.attribs)
+##        print("}")
+##    intt += 1
+print(len(groups))
+
+
+
+
         
 
 
